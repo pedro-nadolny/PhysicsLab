a05ac98577662b021e2c8292d3f6fdfafbfea4c8
Pedro Roberto <pedro.nadolny@gmail.com>
2017-05-20 18:35:23 -0300

    Login Activity Layout.

--- app/src/main/java/robert/peter/physicslab/LoginActivity.java
+++ app/src/main/java/robert/peter/physicslab/LoginActivity.java
@@ -1,7 +1,9 @@
 package robert.peter.physicslab;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+
+    public void presentRegisterActivity (View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
