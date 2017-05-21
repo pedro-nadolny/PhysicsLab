f361ce6a794aba57a57abcf77ae3d3d37a053d80
Pedro Roberto <pedro.nadolny@gmail.com>
2017-05-21 02:08:25 -0300

    Login with firebase.

--- app/src/main/java/robert/peter/physicslab/LoginActivity.java
+++ app/src/main/java/robert/peter/physicslab/LoginActivity.java
@@ -1,21 +1,95 @@
 package robert.peter.physicslab;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = (EditText) findViewById(R.id.email_input);
+        passwordEditText = (EditText) findViewById(R.id.password_input);
+
+        View.OnFocusChangeListener hideKeyboardTouchOutside = new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    hideKeyboard((EditText) v);
+                }
+            }
+        };
+
+        emailEditText.setOnFocusChangeListener(hideKeyboardTouchOutside);
+        passwordEditText.setOnFocusChangeListener(hideKeyboardTouchOutside);
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        if(mAuth.getCurrentUser() != null) {
+            presentRegisterActivity(null);
+        }
     }
 
     public void presentRegisterActivity (View view){
         Intent registerActivity = new Intent(this, RegisterActivity.class);
         startActivity(registerActivity);
+    }
+
+    public void loginAction (View view) {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (email.length() == 0 || password.length() == 0) {
+            Toast.makeText(getApplicationContext(), "Authentication problem.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            if(mAuth.getCurrentUser() != null) {
+                                return;
+                            }
+                        }
+
+                        Toast.makeText(getApplicationContext(), "Authentication problem.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void hideKeyboard(EditText editText) {
+        InputMethodManager imm = (InputMethodManager)getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
     }
 }
--- app/src/main/java/robert/peter/physicslab/RegisterActivity.java
+++ app/src/main/java/robert/peter/physicslab/RegisterActivity.java
@@ -1,20 +1,77 @@
 package robert.peter.physicslab;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText passwordEditText;
+    private EditText passwordConfirmEditText;
+    private EditText emailEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        passwordConfirmEditText = (EditText) findViewById(R.id.confirm_password_input);
+        passwordEditText = (EditText) findViewById(R.id.password_input);
+        emailEditText = (EditText) findViewById(R.id.email_input);
     }
 
-    public void presentLoginActivity(View view) {
-        Intent loginActivity = new Intent(this, LoginActivity.class);
-        startActivity(loginActivity);
+    public void createUser(View view) {
+
+        String password = passwordEditText.getText().toString();
+        String passwordConfirm = passwordConfirmEditText.getText().toString();
+
+        if(!password.equals(passwordConfirm)) {
+            Toast.makeText(getApplicationContext(), "Password dosen't match.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        String email = emailEditText.getText().toString();
+
+        if(email.length() == 0 || password.length() == 0) {
+            Toast.makeText(getApplicationContext(), "Registration failed.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(email.startsWith("@") || email.endsWith("@") || !email.contains("@")) {
+            Toast.makeText(getApplicationContext(), "Registration failed.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Registration failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
