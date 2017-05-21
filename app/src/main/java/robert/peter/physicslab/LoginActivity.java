beaef5c40ece5ab9c230e067056149ca5b313bf8
Pedro Roberto <pedro.nadolny@gmail.com>
2017-05-20 21:23:01 -0300

    Layout register.

--- app/src/main/java/robert/peter/physicslab/LoginActivity.java
+++ app/src/main/java/robert/peter/physicslab/LoginActivity.java
@@ -5,16 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.activity_login);
     }
 
     public void presentRegisterActivity (View view){
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        Intent registerActivity = new Intent(this, RegisterActivity.class);
+        startActivity(registerActivity);
     }
 }
--- app/src/main/java/robert/peter/physicslab/RegisterActivity.java
+++ app/src/main/java/robert/peter/physicslab/RegisterActivity.java
@@ -1,7 +1,9 @@
 package robert.peter.physicslab;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void presentLoginActivity(View view) {
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        startActivity(loginActivity);
     }
 }
